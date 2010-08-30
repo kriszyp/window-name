2a1605e2b705ce752b077433da2475ff90306c50
Kris Zyp <kriszyp@gmail.com>
2010-08-30 13:09:00 -0600

    Initial checkin

---
+++ package.js
@@ -0,0 +1,9 @@
+require.registerPackage({
+	"name": "window-name",
+	"author": "Kris Zyp",
+	"email": "kris@sitepen.com",
+	"mappings": {
+		"xhr-plugins": "http://packages.dojotoolkit.org/xhr-plugins.zip",
+		"secure": "http://packages.dojotoolkit.org/secure.zip"
+	}
+});
