625e8a233a8677192ec77c89f990238c7c8e6e4a
Kris Zyp <kriszyp@gmail.com>
2010-08-30 14:31:49 -0600

    Add license info

--- package.js
+++ package.js
@@ -2,6 +2,7 @@ require.registerPackage({
 	"name": "window-name",
 	"author": "Kris Zyp",
 	"email": "kris@sitepen.com",
+	"license":"AFL/BSD",
 	"mappings": {
 		"xhr-plugins": "http://packages.dojotoolkit.org/xhr-plugins.zip",
 		"secure": "http://packages.dojotoolkit.org/secure.zip"
