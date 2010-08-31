96114fbac42098fb199400ab911edcd01a50a8be
Kris Zyp <kriszyp@gmail.com>
2010-08-31 07:34:33 -0600

    Use suggested aliases for own mappings, use array for licenses

--- package.js
+++ package.js
@@ -2,10 +2,10 @@ require.registerPackage({
 	"name": "window-name",
 	"author": "Kris Zyp",
 	"email": "kris@sitepen.com",
-	"license":"AFL/BSD",
+	"license":["AFL", "BSD"],
 	"cla": "Dojo Foundation",
 	"mappings": {
-		"xhr-plugins": "http://packages.dojotoolkit.org/xhr-plugins.zip",
-		"secure": "http://packages.dojotoolkit.org/secure.zip"
+		"dojo/io": "http://packages.dojotoolkit.org/xhr-plugins.zip",
+		"dojox/secure": "http://packages.dojotoolkit.org/secure.zip"
 	}
 });
