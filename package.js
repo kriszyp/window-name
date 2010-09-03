70fdb8c9ad29605c7ab084b1d0d0662b1aff93d2
Kris Zyp <kriszyp@gmail.com>
2010-09-03 10:35:35 -0600

    Add comments about how package.js could be used in a built package

--- package.js
+++ package.js
@@ -1,3 +1,6 @@
+/* This exists to demonstrate a "built" package that is x-domain accessible, 
+normally a package.js wouldn't be required in the source of a package */
+
 require.registerPackage({
 	"name": "window-name",
 	"author": "Kris Zyp",
@@ -8,4 +11,15 @@ require.registerPackage({
 		"dojo/io": "http://packages.dojotoolkit.org/xhr-plugins.zip",
 		"dojox/secure": "http://packages.dojotoolkit.org/secure.zip"
 	}
+});
+
+/* A built package.js could also (optionally) include any modules that are 
+likely to be needed by the user so that can be provided a single request and 
+don't have to be fetched later.
+Note that this is not an purely anonymous module defintion, but the module id
+should be interpreted relative to the root of the package when a package
+is registered in Dojo */
+
+require.def("window-name", [], ["dojo"], function(dojo){
+//... contents of window-name module ...
 });
